99fc7090940c2800071add994bf4f0842d1b9e22
mpftavares <marianatavares@live.com.pt>
2024-05-07 00:05:45 +0100

    fix: another cleanup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@
 import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<div>Hello, world</div>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
